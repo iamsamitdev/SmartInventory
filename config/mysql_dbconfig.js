ab7c61af3a62782edc6df08d6265aae6904ea819
Samit Koyom <samitkoyom@gmail.com>
2023-10-12 16:06:35 +0700

    update config db

--- config/mysql_dbconfig.js
+++ config/mysql_dbconfig.js
@@ -2,11 +2,21 @@
 const mysql = require('mysql')
 
 // Create MySQL Connection
+// Localhost
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '123456',
+//   database: 'inventorydb',
+//   port: 3306
+// })
+
+// Clever Cloud
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '123456',
-  database: 'inventorydb',
+  host: 'bntrigtltmrz1d4nsjou-mysql.services.clever-cloud.com',
+  user: 'upup0s3fcqie3d5z',
+  password: 'hZ11tZfZqW9kvTia9DGz',
+  database: 'bntrigtltmrz1d4nsjou',
   port: 3306
 })
 
